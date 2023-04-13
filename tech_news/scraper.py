1fd287244c9001529ff9b67dfd61eaca2e990c00
felipe-cruz <piradous@gmail.com>
2023-04-13 14:43:55 -0300

    Req 02 e Req 03 e Parte do 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-# from parsel import Selector
-# from urllib.parse import urlparse
-# import re
+from parsel import Selector
+from urllib.parse import urlparse
+import re
 
 
 # Requisito 1
@@ -21,17 +21,47 @@ def fetch(url: str, wait: int = 3, user_agent="Fake user-agent") -> str:
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    links = []
+    selector = Selector(text=html_content)
+    css_links = selector.css("a[href].cs-overlay-link")
+    for link in css_links:
+        links.append(link.attrib["href"])
+    return links
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        selector = Selector(text=html_content)
+        next_page = selector.css(".next").attrib.get("href")
+        if next_page:
+            return next_page
+        else:
+            raise ValueError("Next page not found")
+    except (ValueError, AssertionError):
+        return None
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    scrape = {}
+    url = html_content.url
+    path = urlparse(url).path
+    category = path.split('/')[1]
+
+    selector = Selector(text=html_content)
+    scrape["url"] = selector.css("li.meta-reading-time::text").get()
+    scrape["title"] = selector.css("h1.entry-title::text").get()
+    scrape["timestamp"] = selector.css("li.meta-date::text").get()
+    scrape["writer"] = selector.css("a.url.fn.n::text").get()
+    reading_time = selector.css("li.meta-reading-time::text").get()
+    tempo_leitura = re.search(r'\d+', reading_time)
+    scrape["reading_time"] = int(tempo_leitura.group())
+    summary = selector.css(".entry-content > p:nth-child(1)::text").get()
+    scrape["summary"] = summary
+    scrape["category"] = category
+
+    return scrape
 
 
 # Requisito 5
