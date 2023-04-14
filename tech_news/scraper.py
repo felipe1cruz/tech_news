88b38cb7213f2dd76ca557f19e0131f9ab5d299d
felipe-cruz <piradous@gmail.com>
2023-04-14 09:17:52 -0300

    req 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,6 @@
 import requests
 from time import sleep
 from parsel import Selector
-from urllib.parse import urlparse
 import re
 
 
@@ -45,20 +44,22 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_news(html_content):
     scrape = {}
-    url = html_content.url
-    path = urlparse(url).path
-    category = path.split('/')[1]
-
     selector = Selector(text=html_content)
-    scrape["url"] = selector.css("li.meta-reading-time::text").get()
-    scrape["title"] = selector.css("h1.entry-title::text").get()
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    scrape["url"] = url
+    scrape["title"] = selector.css("h1.entry-title::text").get().strip()
     scrape["timestamp"] = selector.css("li.meta-date::text").get()
     scrape["writer"] = selector.css("a.url.fn.n::text").get()
     reading_time = selector.css("li.meta-reading-time::text").get()
     tempo_leitura = re.search(r'\d+', reading_time)
     scrape["reading_time"] = int(tempo_leitura.group())
-    summary = selector.css(".entry-content > p:nth-child(1)::text").get()
-    scrape["summary"] = summary
+    summary = "".join(
+        selector.css(
+            "div.entry-content:first-of-type > p:nth-of-type(1) *::text"
+        ).getall()
+    )
+    scrape["summary"] = summary.strip()
+    category = selector.css("span.label::text").get()
     scrape["category"] = category
 
     return scrape
